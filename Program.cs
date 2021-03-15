06538970efd15945b96d5e1354b425f7f6d18e33
Greatzel Unabia <lunabia@ggc.edu>
2021-03-15 17:06:06 -0400

    Jim reference code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGenerator_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
